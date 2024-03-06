d84dc62add5d5424cc553cc138c01c3010f8b7c9
adityabhamare10 <adityabhamare71@gmail.com>
2024-03-06 14:47:31 +0530

    3rd commit

--- app/src/main/java/com/hustler/wastemanagement/MainActivity.java
+++ app/src/main/java/com/hustler/wastemanagement/MainActivity.java
@@ -1,14 +1,55 @@
 package com.hustler.wastemanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Welcome!!");
+
+
+//        if(getSupportActionBar()!=null)
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.opt_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if(itemId==R.id.setting){
+            Toast.makeText(this, "GoingToSetting", Toast.LENGTH_SHORT).show();
+        }else if(itemId == R.id.share){
+            Toast.makeText(this, "Sharing", Toast.LENGTH_SHORT).show();
+        }else if(itemId == android.R.id.home){
+            super.onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
