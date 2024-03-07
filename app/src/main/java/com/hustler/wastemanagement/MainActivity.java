de3aa3e50342dde34fda402708b78a99d462d1d7
adityabhamare10 <adityabhamare71@gmail.com>
2024-03-07 16:01:49 +0530

    4th commit

--- app/src/main/java/com/hustler/wastemanagement/MainActivity.java
+++ app/src/main/java/com/hustler/wastemanagement/MainActivity.java
@@ -1,28 +1,74 @@
 package com.hustler.wastemanagement;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
 
     Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Welcome!!");
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.OpenDrawer,R.string.CloseDrawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.wasteEducation){
+                    Toast.makeText(MainActivity.this, "Education", Toast.LENGTH_SHORT).show();
+                    loadFragment(new EducationFragment());
+                }else if(id == R.id.recyling){
+                    Toast.makeText(MainActivity.this, "Recycling", Toast.LENGTH_SHORT).show();
+                    loadFragment(new RecyclingFragment());
+                }else if(id == R.id.homeMain){
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
 
 
 //        if(getSupportActionBar()!=null)
@@ -30,6 +76,28 @@ public class MainActivity extends AppCompatActivity {
 //
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+
+        FragmentManager fm = getSupportFragmentManager();
+//        fm.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.container, fragment);
+        ft.commit();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,6 +111,8 @@ public class MainActivity extends AppCompatActivity {
 
         if(itemId==R.id.setting){
             Toast.makeText(this, "GoingToSetting", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(i);
         }else if(itemId == R.id.share){
             Toast.makeText(this, "Sharing", Toast.LENGTH_SHORT).show();
         }else if(itemId == android.R.id.home){
